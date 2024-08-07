fd9497a74177c19361f1b541c01bb2f5ffdaf359
rajeshCreate72 <mechdepartment1221@gmail.com>
2024-08-07 23:10:18 +0530

    axios error changes made

--- client/src/components/AdminLogin/AdminLogin.jsx
+++ client/src/components/AdminLogin/AdminLogin.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function AdminLogin() {
   const [email, setEmail] = useState("");
